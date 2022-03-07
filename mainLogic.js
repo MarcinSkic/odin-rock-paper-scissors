cbbac9ac74adef0f19c6eeea4baacb0f2d684f29
MarcinSkic <marcinskicoficjal@gmail.com>
2022-03-07 11:04:37 +0100

    Finish game logic and polish code [CU-21ut6rw]

--- mainLogic.js
+++ mainLogic.js
@@ -1,3 +1,22 @@
+game();
+
+function game(){
+    let score = 0;
+    for(let i = 0; i < 5; i++){
+        let playerSelection = prompt("What symbol you want to use? (Rock, Paper or Scissors)");
+        let computerSelection = computerPlay();
+        let result = playRound(playerSelection,computerSelection);
+        if(isNaN(result)){
+            break;
+        }
+        score += result;
+
+        printResult(result,playerSelection,computerSelection);
+
+        console.log(`Current score: ${score}`);
+    }
+}
+
 function computerPlay(){
     let choice = Math.floor(Math.random()*3);
     switch (choice){
@@ -16,28 +35,37 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     playerSelection = fixPlayerInput(playerSelection);
     if(playerSelection === computerSelection){
-        return 'Draw'
+        return 0;
     } else if(playerSelection === 'Rock'){
-        return checkWinner(playerSelection,computerSelection,'Scissors');
+        return isPlayerWinner(computerSelection,'Scissors');
     } else if(playerSelection === 'Paper'){
-        return checkWinner(playerSelection,computerSelection,'Rock');
+        return isPlayerWinner(computerSelection,'Rock');
     } else if(playerSelection === 'Scissors'){
-        return checkWinner(playerSelection,computerSelection,'Paper');
+        return isPlayerWinner(computerSelection,'Paper');
     } else {
-        return 'Incorrect input!';
+        console.log('Incorrect input!');
+        return undefined;
     }
 }
 
-function checkWinner(playerSelection, computerSelection, loosingComputerSelection){
+function isPlayerWinner(computerSelection, loosingComputerSelection){
     if(computerSelection === loosingComputerSelection){
-        return `You won! ${playerSelection} beats ${computerSelection}`;
+        return 1;
     } else {
-        return `You lost! ${computerSelection} beats ${playerSelection}`;
+        return -1;
+    }
+}
+
+function printResult(result, playerSelection, computerSelection){
+    if(result === 1){
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    } else if(result === -1){
+        console.log(`You lost! ${computerSelection} beats ${playerSelection}`);
+    } else {
+        console.log('Draw');
     }
 }
 
 function fixPlayerInput(playerSelection){
     return `${playerSelection.substring(0,1).toUpperCase()}${playerSelection.substring(1).toLowerCase()}`
 }
-
-console.log(playRound('sciSSors',computerPlay()));
